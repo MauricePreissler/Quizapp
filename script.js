103ed0afa8c5a0779554032a17b95b6d6ba0b899
Maurice Preißler <m.preissler@etik.com>
2024-05-24 19:10:52 +0200

    add fix

--- script.js
+++ script.js
@@ -63,60 +63,55 @@ function init() {
 }
 
 function showQuestion() {
-  if (gameIsOver()) {
-    showEndScreen();
-  } else { 
-    updateProgressBar();
-    updateNextQuestion();
-  }
-}
-function gameIsOver() {
-    return currentQuestion >= questions.length;
-}
-function showEndScreen() {
-    
+  if (currentQuestion >= questions.length) {
+    // TODO: Show End Screen
     document.getElementById('endScreen').style = '';
     document.getElementById('questionBody').style = 'display: none';
 
     document.getElementById('amount-Of-Questions').innerHTML = questions.length;
     document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
     document.getElementById('header-image').src = './img/trophy.png';
-}
-function updateProgressBar() {
+  } else { //Show Question
+
     let percent = (currentQuestion + 1) / questions.length;
     percent = Math.round(percent * 100);
     document.getElementById('progress-bar').innerHTML = `${percent}%`;
     document.getElementById('progress-bar').style = `width: ${percent}%;`;
-}
-function updateNextQuestion() {
+
+    console.log('Fortschritt:', percent);
+
     let question = questions[currentQuestion];
+
     document.getElementById("question-number").innerHTML = currentQuestion + 1;
     document.getElementById("questionText").innerHTML = question["question"];
     document.getElementById("answer_1").innerHTML = question["answer_1"];
     document.getElementById("answer_2").innerHTML = question["answer_2"];
     document.getElementById("answer_3").innerHTML = question["answer_3"];
     document.getElementById("answer_4").innerHTML = question["answer_4"];
+  }
 }
 
 function answer(selection) {
   let question = questions[currentQuestion];
+  console.log("Selected answer is", selection);
   let selectedQuestionNumber = selection.slice(-1);
+  console.log("selectedQuestionNumber is ", selectedQuestionNumber);
+  console.log("Current question is ", question["right_answer"]);
+
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
 
-  if (rightAnswerSelected (selectedQuestionNumber)) {  // Richtige Frage beantwortet
+  if (selectedQuestionNumber == question["right_answer"]) {  // Richtige Frage beantwortet
+    console.log("Richtige Antwort!!");
     document.getElementById(selection).parentNode.classList.add("bg-success");
     AUDIO_SUCCESS.play();
     rightQuestions++;
   } else {
+    console.log("Falsche Antwort");
     document.getElementById(selection).parentNode.classList.add("bg-danger");
     document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
     AUDIO_FAIL.play();
   }
   document.getElementById("next-Button").disabled = false;
-}
-
-function rightAnswerSelected(selectedQuestionNumber) {
-    return selectedQuestionNumber == question["right_answer"]
 }
 
 function nextQuestion() {
@@ -145,6 +140,3 @@ function restartGame() {
     init();
 
 }
-
-
-    
